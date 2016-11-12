ddaec87ffe77c183f027c8f83aabee038198d510
defunSM <sxh8083@g.rit.edu>
2016-11-11 23:58:21 -0500

    major changes and additions

--- src/clojure_ml/core.clj
+++ src/clojure_ml/core.clj
@@ -1,6 +1,7 @@
 (ns clojure-ml.core
   (:use clojure.core.matrix)
-  (:require [clatrix.core :as cl]))
+  (:require [clatrix.core :as cl]
+            [clojure.core.matrix.operators :as M]))
 
 (matrix [[0 1 2] [3 4 5]])
 
@@ -30,5 +31,69 @@
 
 (cl/map-indexed (fn [i j m] i) a)
 
-(defn square-mat []
-  )
+(defn square-mat
+  "Creates a square matrix of size n x n
+whose elements are all e"
+
+  [n e & {:keys [implementation]
+          :or {implementation :persistent-vector}}]
+  (let [repeater #(repeat n %)]
+    (matrix implementation (-> e repeater repeater))))
+
+(square-mat 5 2 :implementation :clatrix)
+
+(defn id-mat
+  "Creates an identity matrix of n x n size."
+  [n]
+  (let [init (square-mat n 0 :implementation :clatrix)
+        identity-f (fn [i j n]
+                     (if (= i j) 1 n))]
+    (cl/map-indexed identity-f init)))
+
+(pm (identity-matrix 5))
+
+
+;; doesn't generate the random matrix you are expecting.
+(defn rand-square-mat
+  "Generates a random matrix."
+  [n]
+  (matrix (repeat n (repeat n (rand-int 100)))))
+
+(rand-square-mat 4)
+
+(defn rand-square-clmat
+  [n]
+  (cl/map (rand-int 100) (square-mat n 100 :implmentation :clatrix)))
+
+(rand-square-clmat 5)
+
+(defn rand-square-mat
+  "Creates a n x n matrix filling with a random number up to r."
+  [n r]
+  (matrix
+   (repeatedly n #(map rand-int (repeat n r)))))
+
+(rand-square-mat 5)
+
+(cl/rnorm 10 25 10 10)
+
+(defn id-computed-mat
+  [n]
+  (compute-matrix [n n] #(if (= %1 %2) 1 0)))
+
+(pm (id-computed-mat 5))
+
+(defn rand-computed-mat
+  [n m]
+  (compute-matrix [n m]
+                  (fn [i j] (rand-int 100))))
+
+(rand-computed-mat 3 10)
+
+(def A (matrix [[0 1 2] [3 4 5]]))
+
+(def B (matrix [[0 0 0] [0 0 0]]))
+
+(M/== B A)
+
+(def C (M/+ A B))
